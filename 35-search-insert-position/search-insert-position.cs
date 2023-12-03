7d2779b5afbda637e03fe4f69f42116646beadcb
Vinicius Mendes Martins <viniciusmmartins@gmail.com>
2023-12-02 23:03:39 -0300

    Time: 78 ms (73.01%) | Memory: 39.3 MB (61.50%) - LeetSync

--- 35-search-insert-position/search-insert-position.cs
+++ 35-search-insert-position/search-insert-position.cs
@@ -1,11 +1,12 @@
 public class Solution {
     public int SearchInsert(int[] nums, int target) {
         int valAnt = -2147483648 ;
-        for(int i = 0; i < nums.Length; i++){
+        int len = nums.Length;
+        for(int i = 0; i < len; i++){
             if (target>valAnt && target <= nums[i])
                 return i;
             valAnt = nums[i];
         }
-        return nums.Length;
+        return len;
     }
 }
